79f2db2d5a7f2e7b3300441d3aad6022267fb30a
trung.daoquoc <trung.daoquoc@gameloft.com>
2025-04-22 18:24:58 +0700

    * add math function for vector draft

--- src/Math/Vector.h
+++ src/Math/Vector.h
@@ -5,4 +5,55 @@ struct Vector
 {
     Type data[size];
 };
+
+template<typename Type, size_t size>
+bool Equal(Vector<Type, size> a, Vector<Type, size> b)
+{
+
+}
+
+template<typename Type, size_t size>
+bool NotEqual(Vector<Type, size> a, Vector<Type, size> b)
+{
+
+}
+
+template<typename Type, size_t size>
+Vector<Type, size> Add(Vector<Type, size> a, Vector<Type, size> b)
+{
+
+}
+
+template<typename Type, size_t size>
+Vector<Type, size> Subtract(Vector<Type, size> a, Vector<Type, size> b)
+{
+
+}
+
+
+template<typename Type, size_t size>
+Vector<Type, size> Mul(Vector<Type, size> a, Vector<Type, size> b)
+{
+
+}
+
+template<typename Type, size_t size>
+Vector<Type, size> Div(Vector<Type, size> a, Vector<Type, size> b)
+{
+
+}
+
+
+template<typename Type, size_t size>
+Vector<Type, size> Normalize(Vector<Type, size> a)
+{
+
+}
+
+template<typename Type, size_t size>
+float Length(Vector<Type, size> a)
+{
+
+}
+
 #endif
